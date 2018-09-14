e5fdf1b9cd398d3103b4df3df762191608a9f84b
Mikel <mikelzuga@hotmail.com>
2018-09-14 13:44:35 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+//Para obtener el puerto || si no existe 3000
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req, res) {
+
+
+    res.render('home', {
+        name: 'Saikiza'
+    });
+})
+app.get('/about', function(req, res) {
+
+    res.render('about');
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
